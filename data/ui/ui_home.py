d3e63286b1bd917a051dcdfde2e945924ee199f1
frrrrrits <canbetterforme@gmail.com>
2023-01-25 20:09:43 +0700

    something went wrong

--- data/ui/ui_home.py
+++ data/ui/ui_home.py
@@ -1,25 +1,26 @@
 # -*- coding: utf-8 -*-
 
 ################################################################################
-## Form generated from reading UI file 'home.ui'
+## Form generated from reading UI file 'new_home.ui'
 ##
 ## Created by: Qt User Interface Compiler version 6.4.2
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
 
-from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+from data.ui.theme.style import *
+
+from PyQt5.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QFont, QFontDatabase, QGradient, QIcon,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QAbstractItemView, QApplication, QFrame, QHBoxLayout,
+from PyQt5.QtWidgets import (QAbstractItemView, QApplication, QFrame, QHBoxLayout,
     QLabel, QListView, QListWidget, QListWidgetItem,
     QMainWindow, QPushButton, QSizePolicy, QVBoxLayout,
     QWidget)
-import images_rc
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -36,12 +37,7 @@ class Ui_MainWindow(object):
         self.drop_shadow_layout.setContentsMargins(10, 10, 10, 10)
         self.drop_shadow_frame = QFrame(self.centralwidget)
         self.drop_shadow_frame.setObjectName(u"drop_shadow_frame")
-        self.drop_shadow_frame.setStyleSheet(u"QFrame#drop_shadow_frame {\n"
-"	border-radius: 10px;\n"
-"	border: 1px solid rgba(189, 189, 189, 120);\n"
-"	background-color: rgb(35, 37, 45);\n"
-"};\n"
-"")
+        self.drop_shadow_frame.setStyleSheet(backgroundFrame)
         self.drop_shadow_frame.setFrameShape(QFrame.NoFrame)
         self.drop_shadow_frame.setFrameShadow(QFrame.Raised)
         self.verticalLayout = QVBoxLayout(self.drop_shadow_frame)
@@ -54,6 +50,8 @@ class Ui_MainWindow(object):
         self.title_bar.setStyleSheet(u"background-color: none;")
         self.title_bar.setFrameShape(QFrame.NoFrame)
         self.title_bar.setFrameShadow(QFrame.Raised)
+
+        # app bar
         self.horizontalLayout = QHBoxLayout(self.title_bar)
         self.horizontalLayout.setSpacing(0)
         self.horizontalLayout.setObjectName(u"horizontalLayout")
@@ -61,10 +59,6 @@ class Ui_MainWindow(object):
         self.frame_title = QFrame(self.title_bar)
         self.frame_title.setObjectName(u"frame_title")
         self.frame_title.setMinimumSize(QSize(0, 50))
-        font = QFont()
-        font.setFamilies([u"Roboto Condensed Light"])
-        font.setPointSize(14)
-        self.frame_title.setFont(font)
         self.frame_title.setFrameShape(QFrame.StyledPanel)
         self.frame_title.setFrameShadow(QFrame.Raised)
         self.verticalLayout_2 = QVBoxLayout(self.frame_title)
@@ -75,16 +69,14 @@ class Ui_MainWindow(object):
         self.label_title.setObjectName(u"label_title")
         font1 = QFont()
         font1.setFamilies([u"Segoe UI Semibold"])
-        font1.setPointSize(14)
+        font1.setPointSize(15)
         font1.setBold(True)
         self.label_title.setFont(font1)
         self.label_title.setStyleSheet(u"color:rgb(255, 255, 255);")
-
         self.verticalLayout_2.addWidget(self.label_title)
-
-
         self.horizontalLayout.addWidget(self.frame_title)
 
+        # context button
         self.frame_btns = QFrame(self.title_bar)
         self.frame_btns.setObjectName(u"frame_btns")
         self.frame_btns.setMaximumSize(QSize(100, 16777215))
@@ -92,21 +84,6 @@ class Ui_MainWindow(object):
         self.frame_btns.setFrameShadow(QFrame.Raised)
         self.horizontalLayout_3 = QHBoxLayout(self.frame_btns)
         self.horizontalLayout_3.setObjectName(u"horizontalLayout_3")
-        self.btn_maximize = QPushButton(self.frame_btns)
-        self.btn_maximize.setObjectName(u"btn_maximize")
-        self.btn_maximize.setMinimumSize(QSize(16, 16))
-        self.btn_maximize.setMaximumSize(QSize(17, 17))
-        self.btn_maximize.setStyleSheet(u"QPushButton {\n"
-"	border: none;\n"
-"	border-radius: 8px;	\n"
-"	background-color: rgb(85, 255, 127);\n"
-"}\n"
-"QPushButton:hover {	\n"
-"	background-color: rgba(85, 255, 127, 150);\n"
-"}")
-
-        self.horizontalLayout_3.addWidget(self.btn_maximize)
-
         self.btn_minimize = QPushButton(self.frame_btns)
         self.btn_minimize.setObjectName(u"btn_minimize")
         self.btn_minimize.setMinimumSize(QSize(16, 16))
@@ -122,6 +99,21 @@ class Ui_MainWindow(object):
 
         self.horizontalLayout_3.addWidget(self.btn_minimize)
 
+        self.btn_maximize = QPushButton(self.frame_btns)
+        self.btn_maximize.setObjectName(u"btn_maximize")
+        self.btn_maximize.setMinimumSize(QSize(16, 16))
+        self.btn_maximize.setMaximumSize(QSize(17, 17))
+        self.btn_maximize.setStyleSheet(u"QPushButton {\n"
+"	border: none;\n"
+"	border-radius: 8px;	\n"
+"	background-color: rgb(85, 255, 127);\n"
+"}\n"
+"QPushButton:hover {	\n"
+"	background-color: rgba(85, 255, 127, 150);\n"
+"}")
+
+        self.horizontalLayout_3.addWidget(self.btn_maximize)
+
         self.btn_close = QPushButton(self.frame_btns)
         self.btn_close.setObjectName(u"btn_close")
         self.btn_close.setMinimumSize(QSize(16, 16))
@@ -136,60 +128,53 @@ class Ui_MainWindow(object):
 "}")
 
         self.horizontalLayout_3.addWidget(self.btn_close)
-
-
         self.horizontalLayout.addWidget(self.frame_btns)
-
-
         self.verticalLayout.addWidget(self.title_bar)
 
+        # main frame
         self.content_bar = QFrame(self.drop_shadow_frame)
         self.content_bar.setObjectName(u"content_bar")
         self.content_bar.setStyleSheet(u"background-color: none;")
-        self.content_bar.setFrameShape(QFrame.StyledPanel)
+        self.content_bar.setFrameShape(QFrame.NoFrame)
         self.content_bar.setFrameShadow(QFrame.Raised)
         self.verticalLayout_4 = QVBoxLayout(self.content_bar)
         self.verticalLayout_4.setObjectName(u"verticalLayout_4")
-        self.AnimeList = QListWidget(self.content_bar)
-        icon = QIcon()
-        icon.addFile(u":/list/ic/image.jpg", QSize(), QIcon.Normal, QIcon.Off)
-        font2 = QFont()
-        font2.setPointSize(11)
-        font2.setBold(False)
-        font2.setStrikeOut(False)
-        font2.setKerning(True)
-        font2.setStyleStrategy(QFont.PreferDefault)
-        __qlistwidgetitem = QListWidgetItem(self.AnimeList)
-        __qlistwidgetitem.setTextAlignment(Qt.AlignLeading|Qt.AlignBottom);
-        __qlistwidgetitem.setFont(font2);
-        __qlistwidgetitem.setIcon(icon);
-        self.AnimeList.setObjectName(u"AnimeList")
-        self.AnimeList.viewport().setProperty("cursor", QCursor(Qt.PointingHandCursor))
-        self.AnimeList.setStyleSheet(u"QListWidget::item {\n"
+        self.anime_list = QListWidget(self.content_bar)
+        self.anime_list.setObjectName(u"anime_list")
+        self.anime_list.setFrameShape(QFrame.NoFrame)
+        self.anime_list.viewport().setProperty("cursor", QCursor(Qt.PointingHandCursor))
+        self.anime_list.setStyleSheet("QListWidget::item {\n"
 "    color: rgb(255, 255, 255);\n"
 "    background-color:transparent;\n"
+"	padding: 7px;\n"
+"}\n"
+"QListView::item:hover {	\n"
+"	border-radius: 10px;\n"
+"    background:rgb(47, 47, 63);\n"
+"}\n"
+"QListView::item:selected {\n"
+"   border-radius: 10px;\n"
+"   background : rgba(62, 64, 90, 120);\n"
 "}\n"
 "QListWidget {\n"
 "    background-color:transparent;\n"
-"}")
-        self.AnimeList.setFrameShape(QFrame.NoFrame)
-        self.AnimeList.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.AnimeList.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
-        self.AnimeList.setIconSize(QSize(170, 170))
-        self.AnimeList.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
-        self.AnimeList.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
-        self.AnimeList.setFlow(QListView.LeftToRight)
-        self.AnimeList.setResizeMode(QListView.Adjust)
-        self.AnimeList.setGridSize(QSize(181, 133))
-        self.AnimeList.setViewMode(QListView.IconMode)
-        self.AnimeList.setModelColumn(0)
+"}\n"
+"QScrollBar:vertical {\n"
+"	border: none;\n"
+"    background: rgb(29, 25, 30);\n"
+" }")
+        self.anime_list.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.anime_list.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.anime_list.setIconSize(QSize(207, 207))
+        self.anime_list.setVerticalScrollMode(QAbstractItemView.ScrollPerPixel)
+        self.anime_list.setHorizontalScrollMode(QAbstractItemView.ScrollPerPixel)
+        self.anime_list.setFlow(QListView.LeftToRight)
+        self.anime_list.setResizeMode(QListView.Adjust)
+        self.anime_list.setGridSize(QSize(242, 163))
+        self.anime_list.setViewMode(QListView.IconMode)
 
-        self.verticalLayout_4.addWidget(self.AnimeList)
-
-
+        self.verticalLayout_4.addWidget(self.anime_list)
         self.verticalLayout.addWidget(self.content_bar)
-
-
         self.drop_shadow_layout.addWidget(self.drop_shadow_frame)
 
         MainWindow.setCentralWidget(self.centralwidget)
@@ -201,25 +186,11 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
-        self.label_title.setText(QCoreApplication.translate("MainWindow", u"Animongo", None))
-#if QT_CONFIG(tooltip)
+        self.label_title.setText(QCoreApplication.translate("MainWindow", u"Animongo Gui", None))
         self.btn_maximize.setToolTip(QCoreApplication.translate("MainWindow", u"Maximize", None))
-#endif // QT_CONFIG(tooltip)
         self.btn_maximize.setText("")
-#if QT_CONFIG(tooltip)
         self.btn_minimize.setToolTip(QCoreApplication.translate("MainWindow", u"Minimize", None))
-#endif // QT_CONFIG(tooltip)
         self.btn_minimize.setText("")
-#if QT_CONFIG(tooltip)
         self.btn_close.setToolTip(QCoreApplication.translate("MainWindow", u"Close", None))
-#endif // QT_CONFIG(tooltip)
         self.btn_close.setText("")
-
-        __sortingEnabled = self.AnimeList.isSortingEnabled()
-        self.AnimeList.setSortingEnabled(False)
-        ___qlistwidgetitem = self.AnimeList.item(0)
-        ___qlistwidgetitem.setText(QCoreApplication.translate("MainWindow", u"KKOKOKOKKKKOOKOKOKOKKOKOKOKKKKOOKOKOKO", None));
-        self.AnimeList.setSortingEnabled(__sortingEnabled)
-
     # retranslateUi
-
